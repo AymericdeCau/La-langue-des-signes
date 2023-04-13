442ea054b23e48c119461ee151f4c4993c235151
AymericdeCau <122289498+AymericdeCau@users.noreply.github.com>
2023-04-13 21:59:02 +0200

    Update home.py

--- home.py
+++ home.py
@@ -1,160 +1,72 @@
 # -*- coding: utf-8 -*-
 import pygame
-from tools import SimpleButton, ComplexButton
-from function import *
-from course import Course
+from tools import *
 
 pygame.init()
 
 clock = pygame.time.Clock()
 clock.tick(60)
 
-pygame.font.init()
-font_h1 = pygame.font.SysFont('helvetic', 150)
-font_h2 = pygame.font.SysFont('helvetic', 120)
-font_h3 = pygame.font.SysFont('helvetic', 90)
-font_p = pygame.font.SysFont('arrial', 30)
-
-pygame.display.set_caption("Le language des signes")
-pygame.display.set_icon(pygame.image.load("./assets/picturesforhome/logo.png"))
-screen = pygame.display.set_mode((1800, 900))
-
-screen_width = screen.get_width()
-screen_height = screen.get_height()
-reload = 2 # 0=nul, 1=rafraichissement, 2=initialisation
-
-running = True
-running_home = bool(importation()[0]) # importation est une fonction définie dans function.py permettant de 
-inventaire = importation()[1]
-running_practice = False
-running_course = False
-running_video = False
-running_settings = False
-
-nut = pygame.image.load("./assets/picturesforhome/nut.png").convert_alpha()
+nut = pygame.image.load("./assets/picturesforhome/nut.png")
 nut = pygame.transform.scale(nut, (150, 150)) # l'écrou pour settings
 
+class Home:
+    def __init__(self, screen):
+        self.screen = screen
 
-while running: # boucle while principle qui permet à la fenêtre de rester ouverte comme sur toute fenêtre pygame
-    # chacune des boucles while situé ici représente une page de l'application
-    # ainsi cette première boucle représente la page d'accueil home sur laquelle est le menu
-    while running_home:
+    def run(self):
+        self.screen.fill(((10, 30, 60)))
+        # toutes les valeurs du menu sont calculés en fonction de ces variables pour facilité le changement de valeurs
+        m_h = self.screen.get_height() / 6 * 5 # hauteur du menu
+        m_L = self.screen.get_width() / 6 * 5 # longeur du menu
+        m_x = self.screen.get_width() / 2 - m_L / 2 # abscisse du menu
+        m_y = self.screen.get_height() / 2 - m_h / 2 # ordonné du menu
+        m_i = 30 # espace entre les éléments du menu
+        
+        # Les cinq boutons du menu sont définis ici, ComplexButton est une class (voir tools.py) facilitant la création et manipulation de ces boutons
+        # home est donc une instance de la class ComplexButton
+        self.home = ComplexButton((0, 210, 250), [(m_x, m_y), (m_x + m_L, m_y), (m_x + m_L, m_y + m_h/20*8), (m_x, m_y + m_h/20*8)])
+        # print(home.__doc__) # <<< pour lire les informations relatives à la classe ComplexButton vous pouvez décommenté l'instruction à gauche de ce texte <<<
+        self.practice = ComplexButton((140, 255, 0), [(m_x , m_y + m_h/20*8 + m_i), (m_x + m_L/5*2, m_y + m_h/20*8 + m_i), (m_x + m_L/5, m_y + m_h), (m_x, m_y + m_h)])
+        self.course = ComplexButton((250, 150, 0), [(m_x + m_L/5*2 + m_i, m_y + m_h/20*8 + m_i), (m_x + m_L, m_y + m_h/20*8 + m_i), (m_x + m_L, m_y + m_h/20*8 + m_i + m_h/20*6  - m_i/2), (m_x + m_L/10*3 + m_i, m_y + m_h/20*8 + m_i + m_h/20*6 - m_i/2)])
+        self.video = ComplexButton((200, 0, 100), [(m_x + m_L/10*3 + m_i - m_i*(m_L/5)/(m_h/20*12-m_i), m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L/10*3 + m_i - m_i*m_L/5/m_h/20*8 + m_L/10*5, m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L/10 + m_i + m_L/5*3, m_y + m_h), (m_x + m_L/5 + m_i, m_y + m_h)])
+        self.settings = ComplexButton((100, 100, 100), [(m_x + m_L/10*3 + 2*m_i - m_i*m_L/5/m_h/20*8 + m_L/10*5 + m_i / 2, m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L, m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L, m_y + m_h), (m_x + m_L/10 + 2*m_i + m_L/5*3 + m_i / 2, m_y + m_h)])
+        
+        self.liste_bouton = [self.home, self.practice, self.course, self.video, self.settings]
+        self.liste_name_bouton = ["home", "practice", "course", "video", "settings"]
 
-        if reload == 2 or screen_width != screen.get_width() or screen_height != screen.get_height():
-            # initialisation de la page quand l'utilisateur ouvre l'application ou quand il modifie la taille de la fenêtre
-
-            screen.fill(((10, 30, 60)))
-            # toutes les valeurs du menu sont calculés en fonction de ces variables pour facilité le changement de valeurs
-            m_h = screen.get_height() / 6 * 5 # hauteur du menu
-            m_L = screen.get_width() / 6 * 5 # longeur du menu
-            m_x = screen.get_width() / 2 - m_L / 2 # abscisse du menu
-            m_y = screen.get_height() / 2 - m_h / 2 # ordonné du menu
-            m_i = 30 # espace entre les éléments du menu
+        for b in self.liste_bouton:
+            # globals() permet de transformer les éléments de la liste liste_bouton de type str en variable
+            b.draw(self.screen) # draw est une méthode définie dans la class bouton qui permet d'afficher les boutons sur la page
             
-            # Les cinq boutons du menu sont définis ici, ComplexButton est une class (voir tools.py) facilitant la création et manipulation de ces boutons
-            # home est donc une instance de la class ComplexButton
-            home = ComplexButton((0, 210, 250), [(m_x, m_y), (m_x + m_L, m_y), (m_x + m_L, m_y + m_h/20*8), (m_x, m_y + m_h/20*8)])
-            # print(home.__doc__) # <<< pour lire les informations relatives à la classe ComplexButton vous pouvez décommenté l'instruction à gauche de ce texte <<<
-            practice = ComplexButton((140, 255, 0), [(m_x , m_y + m_h/20*8 + m_i), (m_x + m_L/5*2, m_y + m_h/20*8 + m_i), (m_x + m_L/5, m_y + m_h), (m_x, m_y + m_h)])
-            course = ComplexButton((250, 150, 0), [(m_x + m_L/5*2 + m_i, m_y + m_h/20*8 + m_i), (m_x + m_L, m_y + m_h/20*8 + m_i), (m_x + m_L, m_y + m_h/20*8 + m_i + m_h/20*6  - m_i/2), (m_x + m_L/10*3 + m_i, m_y + m_h/20*8 + m_i + m_h/20*6 - m_i/2)])
-            video = ComplexButton((200, 0, 100), [(m_x + m_L/10*3 + m_i - m_i*(m_L/5)/(m_h/20*12-m_i), m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L/10*3 + m_i - m_i*m_L/5/m_h/20*8 + m_L/10*5, m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L/10 + m_i + m_L/5*3, m_y + m_h), (m_x + m_L/5 + m_i, m_y + m_h)])
-            settings = ComplexButton((100, 100, 100), [(m_x + m_L/10*3 + 2*m_i - m_i*m_L/5/m_h/20*8 + m_L/10*5 + m_i / 2, m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L, m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L, m_y + m_h), (m_x + m_L/10 + 2*m_i + m_L/5*3 + m_i / 2, m_y + m_h)])
-            
-            liste_bouton = ["home", "practice", "course", "video", "settings"]
+        self.refresh()
 
-            for b in liste_bouton:
-                # globals() permet de transformer les éléments de la liste liste_bouton de type str en variable
-                globals()[b].draw(screen) # draw est une méthode définie dans la class bouton qui permet d'afficher les boutons sur la page
-            reload = 1 # on rajoute les textes et images au dessus
-
-        if reload == 1: # rafraichissement "mineur" de la page home : de tous les textes susceptible d'être survolé puis recouvert par le changement de couleur de fond du bouton
+    def refresh(self):
             text = font_h1.render("La Langue Muette", 1, (255,255,255))
-            screen.blit(text, ((home.coor[0][0] + home.coor[1][0])/2 - text.get_width()/2, (home.coor[1][1] + home.coor[2][1])/2 - text.get_height()/2))
+            self.screen.blit(text, ((self.home.coor[0][0] + self.home.coor[1][0])/2 - text.get_width()/2, (self.home.coor[1][1] + self.home.coor[2][1])/2 - text.get_height()/2))
             text = font_h2.render("Exercice", 1, (255,255,255))
-            screen.blit(text, ((practice.coor[0][0] + (practice.coor[1][0] + practice.coor[2][0])/2)/2 - text.get_width()/2, (practice.coor[1][1] + practice.coor[2][1])/2 - text.get_height()))
+            self.screen.blit(text, ((self.practice.coor[0][0] + (self.practice.coor[1][0] + self.practice.coor[2][0])/2)/2 - text.get_width()/2, (self.practice.coor[1][1] + self.practice.coor[2][1])/2 - text.get_height()))
             text = font_h2.render("Cours", 1, (255,255,255))
-            screen.blit(text, (((course.coor[0][0] + course.coor[3][0])/2 + (course.coor[1][0] + course.coor[2][0])/2)/2 - text.get_width()/2, (course.coor[1][1] + course.coor[2][1])/2 - text.get_height()/2))
+            self.screen.blit(text, (((self.course.coor[0][0] + self.course.coor[3][0])/2 + (self.course.coor[1][0] + self.course.coor[2][0])/2)/2 - text.get_width()/2, (self.course.coor[1][1] + self.course.coor[2][1])/2 - text.get_height()/2))
             text = font_h2.render("Vidéo", 1, (255,255,255))
-            screen.blit(text, (((video.coor[0][0] + video.coor[3][0])/2 + (video.coor[1][0] + video.coor[2][0])/2)/2 - text.get_width()/2, (video.coor[1][1] + video.coor[2][1])/2 - text.get_height()/2))
-            screen.blit(nut, ((settings.coor[0][0] + settings.coor[1][0])/2 - nut.get_width()/2, (settings.coor[1][1] + settings.coor[2][1])/2 - nut.get_height()/2))
-            reload = 0
-
-        for b in liste_bouton: # on vérifie pour chaque bouton du menu
-            if globals()[b].isOver(): # si le bouton est survolé
-                if globals()[b].selection == False : # si le bouton n'est pas encore sélectionner
-                    globals()[b].select(screen) # on change alors la couleur de l'arrière plan
-                    if b == "settings": nut = pygame.transform.rotate(nut, 90) # et si le bouton en question est settings on fait tourner l'écrou
-                    reload = 1 # enfin on rafraichit les textes parceque le changement de couleur se fait par dessus
-                    
+            self.screen.blit(text, (((self.video.coor[0][0] + self.video.coor[3][0])/2 + (self.video.coor[1][0] + self.video.coor[2][0])/2)/2 - text.get_width()/2, (self.video.coor[1][1] + self.video.coor[2][1])/2 - text.get_height()/2))
+            self.screen.blit(nut, ((self.settings.coor[0][0] + self.settings.coor[1][0])/2 - nut.get_width()/2, (self.settings.coor[1][1] + self.settings.coor[2][1])/2 - nut.get_height()/2))
+    
+    def checked(self):
+        for b in self.liste_bouton: # on vérifie pour chaque bouton du menu
+            if b.isOver(): # si le bouton est survolé
+                if b.selection == False : # si le bouton n'est pas encore sélectionner
+                    b.select(self.screen) # on change alors la couleur de l'arrière plan
+                    self.refresh()
+                    if b == self.settings: nut = pygame.transform.rotate(nut, 90) # et si le bouton en question est settings on fait tourner l'écrou
                 pressed = pygame.mouse.get_pressed() # on récupère les cliques de la souris
                 if pressed[0]: # si la souris est préssé alors que le bouton est survolé comme vu au dessus
                     # 0=gauche, 1=milieu, 2=droite
-                    running_home = False # on arrête la boucle actuel
-                    globals()["running_" + b] = True # et on lance la boucle while souhaité
-                    reload = 2 # on met le reload en mode initialisation
+                    print(self.liste_bouton.index(b))
+                    return self.liste_bouton.index(b)
             else:
-                if globals()[b].selection == True : # si le bouton n'est pas survolé mais que la couleur de l'arrière plan est encore en mode sélection alors
-                    globals()[b].unselect(screen) # on le remet en mode non sélection, c'est à dire que l'on remet l'arrière plan initiale
+                if b.selection == True : # si le bouton n'est pas survolé mais que la couleur de l'arrière plan est encore en mode sélection alors
+                    b.unselect(self.screen) # on le remet en mode non sélection, c'est à dire que l'on remet l'arrière plan initiale
+                    self.refresh()
                     if b == "settings": nut = pygame.transform.rotate(nut, 90) # si le bouton en question est le bouton settings on le fait tourner
-                    reload = 1 # enfin on rafraichit les textes parceque le changement de couleur se fait par dessus
-
-
-        pygame.display.flip()  # on rafraichit la page à chaque tour de boucle
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: running, running_home = False, False ; pygame.quit() # l'utilisateur peut fermer la page quand il le souhaite
-                
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False ; pygame.quit()
-
-    while running_practice:
-        screen.blit(pygame.image.load("assets/2.jpg"), (0, 0))
-
-        pygame.display.flip()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                exportation(inventaire)
-                pygame.quit()
-    
-    c = Course()
-
-    # cours_santé = font_h2
-    # cours_action
-    # cours_alimentation
-    # cours_base_et_lieu
-    # cours_famille
-    # cours_santé
-    # cours_
-    title_course = font_h1.render("Cours", 1, (255,255,255))
-    cours_alphabet = font_h2.render("alphabet",1,(255,0,0))
-    list_course = ["Action","Alimentation","base et lieux","Famille","Santé","Sentiments","Temps","Vie en Société"]
-    list_course_picture = ["La-langue-des-signes-main\assets\picturesforcourse\Action.jpg",
-    "La-langue-des-signes-main\assets\picturesforcourse\Alimentation et Divers.jpg",
-    "La-langue-des-signes-main\assets\picturesforcourse\BaseetLieux.jpg",
-    "La-langue-des-signes-main\assets\picturesforcourse\Famille.jpg",
-    "La-langue-des-signes-main\assets\picturesforcourse\Santé.jpg",
-    "La-langue-des-signes-main\assets\picturesforcourse\Sentiments.jpg",
-    "La-langue-des-signes-main\assets\picturesforcourse\Temps.jpg",
-    "La-langue-des-signes-main\assets\picturesforcourse\Vie en Société.jpg"]
-
-    background_course = pygame.image.load(r"C:\La-langue-des-signes-main\La-langue-des-signes-main\assets\picturesforcourse\background.png").convert()
-    while running_course:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                exportation(inventaire)
-                pygame.quit()
-        screen.blit(background_course,(0,0))
-        
-        # affiche et charge les bouttons avec le texte
-        for i in range(len(list_course)):
-            title_of_the_lesson = font_h3.render(list_course[i],1,(255,0,0))
-            cours_affichage = SimpleButton((0,255,0), 750, 300 + i*75, title_of_the_lesson.get_width(), title_of_the_lesson.get_height())
-            if cours_affichage.isOver():
-                cours_affichage = SimpleButton((0,155,0), 750, 300 + i*75, title_of_the_lesson.get_width(), title_of_the_lesson.get_height())
-            cours_affichage.draw(screen)
-            screen.blit(title_of_the_lesson,(750, 300 + i*75))
-        
-        screen.blit(title_course, (750, 120))
-        pygame.display.flip()  # on rafraichie la page régulièrement
+        return False
