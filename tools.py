95455ff0a6ba8cc739a479db09c64bc856e2715a
AymericdeCau <122289498+AymericdeCau@users.noreply.github.com>
2023-04-16 19:07:26 +0200

    mise à jour de niveau mineur

--- tools.py
+++ tools.py
@@ -11,7 +11,7 @@ class SimpleButton:
 
     "La classe SimpleButton permet de créer des >>> boutons rectangulaire <<< de couleur, position, dimension et nom choisis par l'utilisateur lors de sa création"
 
-    def __init__(self, color, x, y, width, height, text='', text_color="black", border_radius=0, color_width=0):
+    def __init__(self, color, x, y, width, height, text='', text_color="black", font=font_h4, border_radius=0, color_width=0):
         # __init__ est une fonction appelé "constructeur" dont le nom est imposé par python, elle se lance lors de la création d'une nouvelle instance de la classe SimpleButton
         # border_radius permet d'arrondir les bords de notre "rectangle"
         # color_width permet de ne faire qu'un trait de largeur color_width autour du texte sans arrière plan (valeur=0 -> bouton plein  et valeur>0 -> ligne autour du texte)
@@ -22,19 +22,21 @@ class SimpleButton:
         self.height = height
         self.text = text
         self.text_color = text_color
+        self.font = font
         self.border_radius = border_radius
         self.color_width = color_width
-        self.selection = False
+        self.selection = True
         # print(self.__doc__) # permet d'afficher la documentation écrit juste au-dessus "La classe SimpleButton ..."
 
-    def draw(self, screen, outline=False):  # il faut appeler draw() pour afficher le bouton dans le script
+    def draw(self, screen, color=10, outline=False):  # il faut appeler draw() pour afficher le bouton dans le script
+        if color == 10: color = self.color
         if outline:
             pygame.draw.rect(screen, outline, 
                              (self.x-2, self.y-2, self.width+4, self.height+4), width=0, border_radius=self.border_radius)
-        pygame.draw.rect(screen, self.color, 
+        pygame.draw.rect(screen, color, 
                          (self.x, self.y, self.width, self.height), self.color_width, border_radius=self.border_radius)
         if self.text != '':
-            text = font_h4.render(self.text, 1, self.text_color)
+            text = self.font.render(self.text, 1, self.text_color)
             screen.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
 
     def isOver(self): # méthode permettant de vérifier si le bouton est survolé
@@ -45,17 +47,17 @@ class SimpleButton:
         return False
     
     def select(self, screen, color="none"): # méthode permettant de changer la couleur du bouton et de modifier l'aparence du curseur
+        pygame.mouse.set_cursor(pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_HAND))
+        self.selection = True
         # Si on ne spécifie pas de couleur, la couleur opossée à celle du bouton est affichée par défaut.
         if color == "none":
             color = (255 - self.color[0], 255 - self.color[1], 255 - self.color[2])
         self.draw(screen, color)
-        pygame.mouse.set_cursor(pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_HAND))
-        self.selection = True
 
     def unselect(self, screen):
+        self.selection = False
         self.draw(screen)
         pygame.mouse.set_cursor(pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_ARROW))
-        self.selection = False
 
 # Un exemple sur une fenêtre nommé screen
 # a = SimpleButton("red",  screen.get_width() / 2 - 125, screen.get_height() /2 - 50, 250, 100, "NomDuBouton", (50, 100, 255), 20,5)
@@ -103,3 +105,26 @@ class ComplexButton:
         self.draw(screen)
         pygame.mouse.set_cursor(pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_ARROW))
         self.selection = False
+
+
+# importation() permet de récupérer les données d'utilisation de l'utilisateur,
+# celle-ci sont stokée dans user_data.txt
+def importation():
+    fichier = open("user_data.txt", "a")
+    fichier.close()
+    with open("user_data.txt", "r") as fichier:
+        inventaire = [ligne.split(" : ") for ligne in fichier]
+    if len(inventaire) == 0:
+        running1 = "yes"
+        inventaire.append(["bienvenue",running1])
+    else: 
+        if inventaire[0][0] == "bienvenue": running1 = inventaire[0][1]
+        else: running1 = "yes"
+    return running1, inventaire
+
+# exportation() est la fonction associé à importation() qui souvegarde
+# les données de l'utilisateur dans le fichier user_data.txt
+def exportation(inventaire):
+    with open("user_data.txt", "w") as fichier:
+        for variable in inventaire:
+            fichier.write(" : ".join(variable))
