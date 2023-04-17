be63d7b106a4c80870d9963bd88550567e525575
AymericdeCau <122289498+AymericdeCau@users.noreply.github.com>
2023-04-17 18:44:12 +0200

    Création de deux boutons

--- practice.py
+++ practice.py
@@ -7,17 +7,23 @@ pygame.init()
 class Practice:
     def __init__(self, screen):
         self.screen = screen
+        self.page = "choose type"
     
     def run(self):
         self.screen.fill(((10, 30, 60)))
-        self.home = SimpleButton((0, 210, 250), 20, 20, 200, 50, "Retour au menu", (255, 255, 255), font_p)
-        self.home.draw(self.screen)
-        self.liste_bouton = [self.home]
-        self.liste_name_bouton = ["home"]
+        if self.page == "choose type":
+            self.home = SimpleButton((0, 210, 250), 20, 20, 200, 50, "Retour au menu", (255, 255, 255), font_p)
+            self.qcm = SimpleButton((250, 150, 0), self.screen.get_width()/5-20, 200, self.screen.get_width()/5+40, self.screen.get_width()/5, "Questionnaire", (255, 255, 255), font_h4, 50)
+            self.jeux = SimpleButton((200, 0, 100), 3*self.screen.get_width()/5-20, 200, self.screen.get_width()/5+40, self.screen.get_width()/5, "Jeux", (255, 255, 255), font_h4, 50, select_color=(255, 210, 50))
+            self.liste_bouton = [self.home, self.qcm, self.jeux]
+            self.liste_bouton_nav = [self.home]
+            self.refresh()
         return 0
     
     def refresh(self):
         self.home.draw(self.screen)
+        self.qcm.draw(self.screen)
+        self.jeux.draw(self.screen)
 
     def checked(self):
         for b in self.liste_bouton:
@@ -27,7 +33,7 @@ class Practice:
                 pressed = pygame.mouse.get_pressed() # on récupère les cliques de la souris
                 if pressed[0]: # si la souris est préssé alors que le bouton est survolé comme vu au dessus
                     # 0=gauche, 1=milieu, 2=droite
-                    return self.liste_bouton.index(b)
+                    if b in self.liste_bouton_nav: return self.liste_bouton.index(b)
             else:
                 if b.selection: # si le bouton n'est pas survolé mais que la couleur de l'arrière plan est encore en mode sélection alors
                     b.unselect(self.screen) # on le remet en mode non sélection, c'est à dire que l'on remet l'arrière plan initiale
