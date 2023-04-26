15c344138a6c40dcc0a3865c6e08f261042cef09
AymericdeCau <122289498+AymericdeCau@users.noreply.github.com>
2023-04-26 22:14:29 +0200

    Add files via upload

---
+++ Application_LaLangueMuette/home.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import pygame
+from tools import *
+
+pygame.init()
+
+class Home:
+
+    def __init__(self, screen):
+        self.screen = screen
+        nut = pygame.image.load("./assets/picturesforhome/nut.png")
+        self.nut = pygame.transform.scale(nut, (150, 150)) # l'écrou pour settings
+
+    def run(self):
+        self.screen.fill(((10, 30, 60)))
+        # toutes les valeurs du menu sont calculés en fonction de ces variables pour facilité le changement de valeurs
+        m_h = self.screen.get_height() / 6 * 5 # hauteur du menu
+        m_L = self.screen.get_width() / 6 * 5 # longeur du menu
+        m_x = self.screen.get_width() / 2 - m_L / 2 # abscisse du menu
+        m_y = self.screen.get_height() / 2 - m_h / 2 # ordonné du menu
+        m_i = 30 # espace entre les éléments du menu
+        
+        # Les cinq boutons du menu sont définis ici, ComplexButton est une class (voir tools.py) facilitant la création et manipulation de ces boutons
+        # home est donc une instance de la class ComplexButton
+        self.home = ComplexButton((0, 210, 250), [(m_x, m_y), (m_x + m_L, m_y), (m_x + m_L, m_y + m_h/20*8), (m_x, m_y + m_h/20*8)])
+        # print(home.__doc__) # <<< pour lire les informations relatives à la classe ComplexButton vous pouvez décommenté l'instruction à gauche de ce texte <<<
+        self.practice = ComplexButton((140, 255, 0), [(m_x , m_y + m_h/20*8 + m_i), (m_x + m_L/5*2, m_y + m_h/20*8 + m_i), (m_x + m_L/5, m_y + m_h), (m_x, m_y + m_h)])
+        self.course = ComplexButton((250, 150, 0), [(m_x + m_L/5*2 + m_i, m_y + m_h/20*8 + m_i), (m_x + m_L, m_y + m_h/20*8 + m_i), (m_x + m_L, m_y + m_h/20*8 + m_i + m_h/20*6  - m_i/2), (m_x + m_L/10*3 + m_i, m_y + m_h/20*8 + m_i + m_h/20*6 - m_i/2)])
+        self.video = ComplexButton((200, 0, 100), [(m_x + m_L/10*3 + m_i - m_i*(m_L/5)/(m_h/20*12-m_i), m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L/10*3 + m_i - m_i*m_L/5/m_h/20*8 + m_L/10*5, m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L/10 + m_i + m_L/5*3, m_y + m_h), (m_x + m_L/5 + m_i, m_y + m_h)], select_color=(255, 210, 50))
+        self.settings = ComplexButton((100, 100, 100), [(m_x + m_L/10*3 + 2*m_i - m_i*m_L/5/m_h/20*8 + m_L/10*5 + m_i / 2, m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L, m_y + m_h/20*8 + m_i + m_h/20*6 + m_i/2), (m_x + m_L, m_y + m_h), (m_x + m_L/10 + 2*m_i + m_L/5*3 + m_i / 2, m_y + m_h)])
+        
+        self.liste_bouton = [self.home, self.practice, self.course, self.video, self.settings]
+        self.liste_name_bouton = ["home", "practice", "course", "video", "settings"]
+
+        for b in self.liste_bouton:
+            # globals() permet de transformer les éléments de la liste liste_bouton de type str en variable
+            b.draw(self.screen) # draw est une méthode définie dans la class bouton qui permet d'afficher les boutons sur la page
+            
+        self.refresh()
+        return 0
+
+    def refresh(self):
+            text = font_h1.render("La Langue Muette", 1, (255,255,255))
+            self.screen.blit(text, ((self.home.coor[0][0] + self.home.coor[1][0])/2 - text.get_width()/2, (self.home.coor[1][1] + self.home.coor[2][1])/2 - text.get_height()/2))
+            text = font_h2.render("Exercice", 1, (255,255,255))
+            self.screen.blit(text, ((self.practice.coor[0][0] + (self.practice.coor[1][0] + self.practice.coor[2][0])/2)/2 - text.get_width()/2, (self.practice.coor[1][1] + self.practice.coor[2][1])/2 - text.get_height()))
+            text = font_h2.render("Cours", 1, (255,255,255))
+            self.screen.blit(text, (((self.course.coor[0][0] + self.course.coor[3][0])/2 + (self.course.coor[1][0] + self.course.coor[2][0])/2)/2 - text.get_width()/2, (self.course.coor[1][1] + self.course.coor[2][1])/2 - text.get_height()/2))
+            text = font_h2.render("Traducteur", 1, (255,255,255))
+            self.screen.blit(text, (((self.video.coor[0][0] + self.video.coor[3][0])/2 + (self.video.coor[1][0] + self.video.coor[2][0])/2)/2 - text.get_width()/2, (self.video.coor[1][1] + self.video.coor[2][1])/2 - text.get_height()/2))
+            self.screen.blit(self.nut, ((self.settings.coor[0][0] + self.settings.coor[1][0])/2 - self.nut.get_width()/2, (self.settings.coor[1][1] + self.settings.coor[2][1])/2 - self.nut.get_height()/2))
+    
+    def checked(self):
+        for b in self.liste_bouton: # on vérifie pour chaque bouton du menu
+            if b.isOver(): # si le bouton est survolé
+                if b.selection == False : # si le bouton n'est pas encore sélectionner
+                    b.select(self.screen) # on change alors la couleur de l'arrière plan
+                    if b == self.settings: self.nut = pygame.transform.rotate(self.nut, 90) # et si le bouton en question est settings on fait tourner l'écrou
+                    self.refresh()
+                pressed = pygame.mouse.get_pressed() # on récupère les cliques de la souris
+                if pressed[0]: # si la souris est préssé alors que le bouton est survolé comme vu au dessus
+                    # 0=gauche, 1=milieu, 2=droite
+                    return self.liste_bouton.index(b)
+            else:
+                if b.selection == True : # si le bouton n'est pas survolé mais que la couleur de l'arrière plan est encore en mode sélection alors
+                    b.unselect(self.screen) # on le remet en mode non sélection, c'est à dire que l'on remet l'arrière plan initiale
+                    self.refresh()
+                    if b == "settings": self.nut = pygame.transform.rotate(self.nut, 90) # si le bouton en question est le bouton settings on le fait tourner
+        return False
+# Ce document est sous licence Creative Commons CC BY-SA
---
+++ Application_LaLangueMuette/main.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+import pygame
+from tools import *
+from home import Home
+from course import Course
+from practice import Practice
+from settings import Settings
+#from hand_detector import SignDetector
+
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+clock.tick(fps)
+
+pygame.display.set_caption("La Langue Muette")
+pygame.display.set_icon(pygame.image.load("./assets/picturesforhome/logo.png"))
+screen = pygame.display.set_mode((1800, 900))
+
+screen_width = screen.get_width()
+screen_height = screen.get_height()
+reload = 2 # 0=nul, 1=rafraichissement, 2=initialisation
+
+running = True
+running_home = True
+running_practice = False
+running_course = False
+running_video = False
+running_settings = False
+inventory = importation()
+liste_name_page = ["home", "practice", "course", "video", "settings"]
+
+while running: # boucle while principle qui permet à la fenêtre de rester ouverte comme sur toute fenêtre pygame
+
+
+    for event in pygame.event.get(): # pour pouvoir sortir quand on est dans aucune des pages
+        if event.type == pygame.QUIT:
+            running = False ; pygame.quit()
+
+
+    # chacune des boucles while situé ici représente une page de l'application
+    # ainsi cette première boucle représente la page d'accueil home sur laquelle est le menu
+    while running_home:
+        if reload == 2: # initialisation de la page quand l'utilisateur l'ouvre
+            menu = Home(screen) # on crée notre menu
+            reload = menu.run() # on le lance
+        
+        index_of_next_page = menu.checked() # on vérifie si l'utilisateur clique sur un des boutons
+        if index_of_next_page != False: # si un des boutons est cliqué
+            running_home = False # on ferme la page du menu
+            reload = 2 # on met reload en mode initialisation pour l'initiatlisation de la prochaine page
+            globals()["running_" + liste_name_page[index_of_next_page]] = True # on lance la page qui a été cliqué
+
+        pygame.display.flip()  # on rafraichit la page à chaque tour de boucle
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: running, running_home = False, False ; pygame.quit() # l'utilisateur peut fermer la page quand il le souhaite
+
+
+    while running_practice:
+        if reload == 2: # initialisation de la page quand l'utilisateur l'ouvre
+            practice = Practice(screen, inventory) # on crée notre menu
+            reload = practice.run() # on le lance
+
+        index_of_next_page, inventory = practice.checked() # on vérifie si l'utilisateur clique sur un des boutons menant à une autre page
+        if index_of_next_page != "none": # si un des boutons est cliqués
+            running_practice = False # on ferme la page du menu
+            reload = 2 # on met reload en mode initialisation pour l'initiatlisation de la prochaine page
+            globals()["running_" + liste_name_page[index_of_next_page]] = True # on lance la page qui a été cliquée
+
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running, running_practice = False, False
+                export((inventory))
+                pygame.quit()
+
+    while running_settings:
+        if reload == 2: # initialisation de la page quand l'utilisateur l'ouvre
+            settings = Settings(screen, 60) # on crée notre menu
+            reload = settings.run() # on le lance
+
+        index_of_next_page, fps = settings.checked() # on vérifie si l'utilisateur clique sur un des boutons
+        clock.tick(fps)
+        print("fps :", fps)
+        if index_of_next_page != "none": # si un des boutons est cliqués
+            running_settings = False # on ferme la page du menu
+            reload = 2 # on met reload en mode initialisation pour l'initiatlisation de la prochaine page
+            globals()["running_" + liste_name_page[index_of_next_page]] = True # on lance la page qui a été cliquée
+
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: running, running_settings = False, False ; pygame.quit()
+
+    while running_course: # initialisation de la page quand l'utilisateur l'ouvre
+        if reload == 2:
+            course = Course(screen)
+            reload = course.run(True,False)
+        running_course = False
+        reload = 2
+        running_home = True
+    onclick = True
+    while running_video:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: running, running_video = False, False ; pygame.quit()
+        if reload == 2:
+            screen.fill(((10, 30, 60)))
+            title = font_h1.render("Détecteur de signe",1,(255, 0, 0))
+            explanations1 = font_p.render("Ce programme est conçu pour détecter l'alphabet FSL à partir de la vidéo de votre webcam. Nous souhaitons",1,(255,)*3)
+            explanations2 = font_p.render("vous informer que ce programme ne collecte ni n'envoie aucune donnée de votre webcam.Nous vous recommandons de ",1,(255,)*3)
+            explanations3 = font_p.render("mettre votre main devant la caméra avant de lancer le programme afin de garantir le bon lancement du programme.",1,(255,)*3)
+            explanations4 = font_p.render("Nous espérons que cela vous permettra de profiter pleinement de notre programme en toute sécurité. Merci",1,(255,)*3)
+            accepter = SimpleButton((255,155,0), 800, 700, 250, 130,"Accepter",(255,)*3,font_h4)
+            if accepter.isOver() :
+                accepter = SimpleButton((255,0,255), 800, 700, 250, 130,"Accepter",(255,)*3,font_h4)
+                pressed = pygame.mouse.get_pressed() # on récupère les cliques de la souris
+                if pressed[0] and not onclick:
+                    retour = font_p.render("Cliquez sur Échap sur cette page pour sortir",1, (255, 255, 255))
+                    printing_alphabet_lesson = pygame.image.load("./assets/picturesforcourse/alphabetFSL.png").convert()
+                    screen.blit(retour, (900 - retour.get_width()//2, 15))
+                    screen.blit(printing_alphabet_lesson,(900 - printing_alphabet_lesson.get_width()//2, 450 - printing_alphabet_lesson.get_height()//2 + 30)) # affiche au centre l’image
+                    pygame.display.flip()  # on rafraichie la page 
+
+                    SDetector = SignDetector(screen)
+                    reload = SDetector.run(True)
+                    while reload:
+                        running_video = False
+                        break
+            pressed = pygame.mouse.get_pressed() # on récupère les cliques de la souris
+            if not pressed[0]:
+                onclick = False
+            screen.blit(title,(900 - title.get_width()//2, 60))
+            screen.blit(explanations1,(900 - explanations1.get_width()//2,220))
+            screen.blit(explanations2,(900 - explanations2.get_width()//2,300))
+            screen.blit(explanations3,(900 - explanations3.get_width()//2,380))
+            screen.blit(explanations4,(900 - explanations4.get_width()//2,460))
+            accepter.draw(screen)
+
+            
+            
+            pygame.display.flip()  # on rafraichie la page 
+
+        
+        reload = 2
+        running_home = True
+
+# Ce document est sous licence Creative Commons CC BY-SA
